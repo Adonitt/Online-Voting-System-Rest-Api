7472e1adf5433d09a2ecdeef4300aac7da5a0769
Adonit Halili <adonit.ha12@gmail.com>
2025-04-21 15:26:00 +0200

    first commit

---
+++ src/test/java/org/example/kqz/KqzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kqz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KqzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
