e26b84f2fd7fa83828fd0f3bea13f1d7149bd25e
Fatlind Gashi <fatlindgashi88@gmail.com>
2025-06-07 00:33:39 +0200

    added postmapping for "/forgot-password" also permission in security given

--- src/main/java/org/example/kqz/controllers/UserController.java
+++ src/main/java/org/example/kqz/controllers/UserController.java
@@ -6,18 +6,23 @@ import org.example.kqz.dtos.user.CreateUserRequestDto;
 import org.example.kqz.dtos.user.UpdateUserRequestDto;
 import org.example.kqz.dtos.user.UserDetailsDto;
 import org.example.kqz.dtos.user.UserListingDto;
+import org.example.kqz.repositories.UserRepository;
 import org.example.kqz.services.interfaces.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService service;
+    private final UserRepository userRepository;
 
     @GetMapping("")
     public ResponseEntity<List<UserListingDto>> findAll() {
@@ -50,6 +55,21 @@ public class UserController {
     public CreateUserRequestDto defaultDriver() {
         return new CreateUserRequestDto();
     }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<Map<String, String>> forgotPassword(@RequestBody Map<String, String> request) {
+        String email = request.get("email").trim().toLowerCase();
+        boolean emailExists = userRepository.existsByEmail(email);
+
+        if (!emailExists) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("error", "Email does not exist in our system."));
+        }
+
+        return ResponseEntity.ok()
+                .body(Map.of("success", "Reset instructions sent to your email."));
+    }
+
 
 
 
