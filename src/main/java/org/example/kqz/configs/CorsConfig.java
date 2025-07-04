a7d845712afe19e3d30234225ed95daccb8df0e7
Adonit Halili <adonit.ha12@gmail.com>
2025-07-04 20:32:07 +0200

    fixed cors config

--- src/main/java/org/example/kqz/configs/CorsConfig.java
+++ src/main/java/org/example/kqz/configs/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://your-app-name.netlify.app"
+                "https://adonithalili-vote-online-sysyem.netlify.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
