8ca9bbc26020588d99af566a87434c4984440d02
Adonit Halili <adonit.ha12@gmail.com>
2025-07-08 18:44:47 +0200

    solved

--- src/main/java/org/example/kqz/controllers/VotingDatesController.java
+++ src/main/java/org/example/kqz/controllers/VotingDatesController.java
@@ -32,10 +32,6 @@ public class VotingDatesController {
         return ResponseEntity.ok(votingDatesService.updateVotingDates(id, votingDates));
     }
 
-    @GetMapping("/current")
-    public ResponseEntity<VotingDates> getCurrent() {
-        return ResponseEntity.ok(votingDatesService.getCurrentVotingDates());
-    }
 
     @GetMapping("/default")
     public VotingDates defaultObject() {
